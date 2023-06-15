1c73c12c64e0b914757264aefbacaf7444431862
Chloeliyi <s10247578@connect.np.edu.sg>
2023-06-15 21:24:22 +0800

    Updated code for teleport

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -5,6 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class Teleport : MonoBehaviour
 {
+
+    static Vector3 Playerpos;
+    public int Level1;
+    public int Level2;
+    //public Transform newPos;
+    public Transform Player;
+
+    private void OnCollisionEnter(Collision door)
+    {
+        if(door.gameObject.tag == "exit")
+
+            //Playerpos = newPos.position;
+
+        {
+            Debug.Log("Exit");
+            SceneManager.LoadScene(Level2);
+            //Player.position = newPos.position;
+            Debug.Log(Player.position);
+            Playerpos = Player.position;
+        }
+
+        else if(door.gameObject.tag == "entrance")
+        {
+            Debug.Log("Entrance");
+            SceneManager.LoadScene(Level1);
+            //Player.position = Playerpos;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
