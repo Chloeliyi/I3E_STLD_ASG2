6966d090d8633b90a68b5fb61074dc6a4310456a
Chloeliyi <s10247578@connect.np.edu.sg>
2023-07-02 23:13:15 +0800

    changed code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
-using UnityEditor.Audio;
+using UnityEngine.Audio;
 
 /// <summary>
 /// Controls player movement
