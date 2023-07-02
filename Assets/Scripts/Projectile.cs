91fe9887f6d6b5571aefe736d0ab06b9cb01716e
Chloeliyi <s10247578@connect.np.edu.sg>
2023-07-02 22:53:41 +0800

    projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class to destroy projectile if it hits ground or house
+/// </summary>
 public class Projectile : MonoBehaviour
 {
     public void DestroyProjectiles()
@@ -19,6 +22,16 @@ public class Projectile : MonoBehaviour
             Debug.Log("Ground collision");
             Destroy(gameObject);
         }
+        else if (collision.gameObject.tag == "house")
+        {
+            Debug.Log("House collision");
+            Destroy(gameObject);
+        }
+        /*else if (collision.gameObject.tag == "test")
+        {
+            Debug.Log("Object collision");
+            Destroy(gameObject);
+        }*/
     }
 
     // Start is called before the first frame update
